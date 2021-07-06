66195bfde60b6484592ec603902995e8857b68f2
Alex <lialex0218@gmail.com>
2021-07-05 18:23:44 -0700

    using classnames library for refractoring

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,13 @@ import React from "react";
 
 import "components/Button.scss";
 import { action } from "@storybook/addon-actions/dist/preview";
+import classnames from "classnames"
 
 export default function Button(props) {
-   let buttonClass = "button";
- 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   } 
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   const buttonClass = classnames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
 
 
    return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
