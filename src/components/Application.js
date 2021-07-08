e8e5b15f8328292f39d76013920855a278ed5db7
Alex <lialex0218@gmail.com>
2021-07-07 18:21:14 -0700

    requesting days added to take data from api days to put on the nav bar

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import DayList from "components/DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+const axios = require("axios");
+
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+
+
+
 
 const appointments = [
   {
@@ -64,7 +71,20 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
+  const [days, setDays] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8001/api/days`)
+      .then(response => {
+        console.log(response.data);
+        setDays([...response.data])
+        
+      })
+  }, [])
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -77,8 +97,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={day}
-            setDay={setDay}
+            day={days}
+            setDay={setDays}
           />
         </nav>
         <img
