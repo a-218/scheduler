a92debc53f0b5ea0b2ca728ef581566dddf9b906
Alex <lialex0218@gmail.com>
2021-07-08 18:44:28 -0700

    add in tranistion custom hook with on cancel button that backs

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,14 +5,40 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "./Form";
+
+import useVisualMode from "hooks/useVisualMode";
+
+
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 
 export default function Appointment(props) {
-  
- return (<article className="appointment">
-      <Header time ={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty onClick={props.onAdd}/>}
-      </article>
- )
+
+     const { mode, transition, back } = useVisualMode(
+          props.interview ? SHOW : EMPTY
+     );
+
+     console.log('***************', props);
+
+
+     return (<article className="appointment">
+          <Header time={props.time} />
+          {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+          {mode === SHOW &&
+               (<Show student={props.interview.student} 
+               interviewer={props.interview.interviewer}
+      
+               />)
+          }
+          {mode === CREATE && ( <Form interviewers={[]} onCancel={back} />)}
+
+         
+     </article>
+     )
 };
-    
+
 
