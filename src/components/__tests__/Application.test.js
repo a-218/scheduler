69281eb0279be868bc5ef8313decb975ae2d4672
Alex <lialex0218@gmail.com>
2021-07-13 19:26:25 -0700

    the application test fix warming after adding another await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText,prettyDOM,getAllByTestId,getByAltText,getByPlaceholderText,queryByText} from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 import axios from "axios";
@@ -10,34 +10,40 @@ afterEach(cleanup);
 describe("appointment", () => {
   it("changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
-  
+
     await waitForElement(() => getByText("Monday"));
-  
+
     fireEvent.click(getByText("Tuesday"));
-  
+
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
-
-
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container} = render(<Application />);
+    const { container, debug } = render(<Application />);
   
     await waitForElement(() => getByText(container, "Archie Cohen"));
   
     const appointments = getAllByTestId(container, "appointment");
-
     const appointment = appointments[0];
-
+  
     fireEvent.click(getByAltText(appointment, "Add"));
   
-    fireEvent.change(getByPlaceholderText(appointment, /Enter Student Name/i), {
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
   
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
+  
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
-    console.log(prettyDOM(appointment));
+  
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+
 });
